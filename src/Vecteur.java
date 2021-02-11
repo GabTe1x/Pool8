6eae895ff1a3619b3fe6ce9b02c319ff84606e9c
Gabriel Teixeira <gt75.196@gmail.com>
2021-02-11 21:30:23 +0100

    update

--- src/Vecteur.java
+++ src/Vecteur.java
@@ -1,8 +1,27 @@
 public class Vecteur{
-	Bille b1;
-	Bille b2;
 
-	public Vecteur(Bille b,Bille b2){
-		b1=b;
+	// Point d'origine du vecteur 
+
+	private Point p1;
+
+	// Angle du vecteur déterminé par le point d'origine et la bille blanche
+
+	private int direction;
+
+	public Vecteur(Point p1, int d){
+		this.p1=p1;
+		direction=d;
+	}
+
+	//Récupère le point d'origine du vecteur
+
+	public Point getPoint(){
+		return this.p1;
+	}
+
+	//Récupère l'angle du vecteur 
+
+	public int getDirection(){
+		return direction;
 	}
 }
