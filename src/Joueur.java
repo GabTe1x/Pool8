3ac58b93ba420bb6ccef69548480648938f162e7
TRAN marilyn <marilyn24@hotmail.fr>
2021-02-26 15:01:51 +0100

    Update src/Joueur.java

---
+++ src/Joueur.java
@@ -0,0 +1,29 @@
+
+public class Joueur {
+	
+	private String pseudo;
+	private int score;
+	private boolean gagne;
+	
+	public String getPseudo() {
+		return pseudo;
+	}
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public boolean isGagne() {
+		return gagne;
+	}
+	public void setGagne(boolean gagne) {
+		this.gagne = gagne;
+	}
+	
+	
+	
+}
