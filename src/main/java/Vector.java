c6668b5b51cb68f7b378df1353f84666ea2eb7fe
Gabriel Teixeira <gt75.196@gmail.com>
2021-05-16 17:29:47 +0200

    Private

--- src/main/java/Vector.java
+++ src/main/java/Vector.java
@@ -2,8 +2,8 @@
 
 public class Vector {
 
-    double x;
-    double y;
+    private double x;
+    private double y;
 
     public Vector(){
         x=0;
@@ -14,4 +14,22 @@ public class Vector {
         this.x=x;
         this.y=y;
     }
+
+
+    double getX(){
+        return this.x;
+    }
+
+    void setX(double x1){
+        this.x = x1;
+    }
+
+    double getY(){
+        return this.y;
+    }
+
+    void setY(double y1){
+        this.y = y1;
+    }
+
 }
